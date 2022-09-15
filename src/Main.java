c8a1ead1429e091e46892e906b51bd203900264d
drewspampy22 <drewspampy22@gmail.com>
2022-09-15 15:06:14 -0400

    Seasonal_Cost

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double FallCost = 300;
+        double WinterCost = 205.5;
+        double SpringCost = 400;
+        double SummerCost = 450;
+        double TotalCost = FallCost + WinterCost + SpringCost + SummerCost;
+        System.out.println("The Fall Cost is " + FallCost);
+        System.out.println("The Winter Cost is " + WinterCost);
+        System.out.println("The Spring Cost is " + SpringCost);
+        System.out.println("The Summer Cost is " + SummerCost);
+        System.out.println("The total cost for four seasons is " + TotalCost );
+    }
+}
